677f2636b93fd2647ffc7054d24f3654fbb232aa
Steven Chuinard <steven.chuinard@outlook.fr>
2017-09-22 18:51:18 +0200

    Ajoutez des fichiers projet.

---
+++ EAnim/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EAnim.Startup))]
+namespace EAnim
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
